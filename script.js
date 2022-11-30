7f1330fc56e8118b651f7f6f85127634b13b03a0
azad <azad.shahbazov93@gmail.com>
2022-12-01 01:20:59 +0400

    changes

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let myDiv = document.getElementById('textdiv')
 let clear = document.querySelector('.clear')
 let plus = document.querySelector('.mybutton2')
 var icon = document.querySelector('#myicon')
+
+
 let arr = []
 
 myButton.addEventListener('click', ()=>{
     myDiv.style.display = 'block'
     arr.push(myInput.value)
-    myText.innerHTML += `<li>${myInput.value}</li>`
+    myText.innerHTML += `<li>${myInput.value}</li><img class="clear2" src ="./src/Group77.png"/>`
    
 
 })
@@ -19,10 +21,11 @@ document.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         myDiv.style.display = 'block'
         arr.push(myInput.value)
-        myText.innerHTML += `<li>${myInput.value}</li>`
-    
+        myText.innerHTML += `<li>${myInput.value}</li><img id="clear2" src ="./src/Group77.png"/>`
+        
     }
 });
+
 
 clear.addEventListener('click', ()=>{
     myInput.style.display = 'none'
@@ -38,14 +41,23 @@ plus.addEventListener('click', ()=>{
 
 icon.addEventListener('click', (e)=>{
     
-    var src = e.target.src
-    if(src == "/src/Group73.png"){
-        icon = "/src/Group91.png"
+    var src = e.target.src.split('/')
+    
+    if(src[src.length-1] == "Group73.png"){
+        icon.src = "./src/Group91.png"
         arr.sort()
+        myText.innerHTML = ''
+        arr.forEach((item,index)=>{
+            myText.innerHTML += `<li>${item}</li`
+        })
+        
+    }else {
+        icon.src = "./src/Group73.png"
+        arr.reverse()
+        myText.innerHTML = ''
         arr.forEach((item,index)=>{
             myText.innerHTML += `<li>${item}</li>`
         })
-        console.log('hahaa')
     }
     
 })
