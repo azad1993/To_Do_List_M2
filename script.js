4fcfe9c0053c5859b6d8216a1216b34426e1836b
azad <azad.shahbazov93@gmail.com>
2022-12-03 20:14:07 +0400

    changes

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let arr = [];
 
 
 var myArr = localStorage.getItem("array").split(",");
+arr= myArr
 console.log(myArr)
 myArr.forEach((item, index) => {
   if(myArr[index] != ""){
@@ -20,6 +21,7 @@ myText.innerHTML += `<img id="clear${index}" src ="./src/Group77.png"/><li>${ite
 });
 
 myButton.addEventListener("click", () => {
+ 
   if (myInput.value) {
     count++;
     myDiv.style.display = "block";
@@ -30,6 +32,7 @@ myButton.addEventListener("click", () => {
 });
 
 document.addEventListener("keypress", function (e) {
+
   if (myInput.value) {
     if (e.key === "Enter") {
       count++;
@@ -54,40 +57,39 @@ plus.addEventListener("click", () => {
 
 icon.addEventListener("click", (e) => {
   var src = e.target.src.split("/");
-  myArr = localStorage.getItem("array").split(",");
+
   if (src[src.length - 1] == "Group73.png") {
     icon.src = "./src/Group91.png";
-    myArr.sort();
+    arr.sort();
     myText.innerHTML = "";
-    myArr.forEach((item, index) => {
+    arr.forEach((item, index) => {
       myText.innerHTML += `<img id="clear${index}" src ="./src/Group77.png"/><li>${item}</li>`;
     });
-    localStorage.setItem("array", myArr);
+    localStorage.setItem("array", arr);
   } else {
     icon.src = "./src/Group73.png";
-    myArr.reverse();
+    arr.reverse();
     myText.innerHTML = "";
-    myArr.forEach((item, index) => {
+    arr.forEach((item, index) => {
       myText.innerHTML += `<img id="clear${index}" src ="./src/Group77.png"/><li>${item}</li>`;
       
     });
-    localStorage.setItem("array", myArr);
+    localStorage.setItem("array", arr);
   }
 });
 
 myText.addEventListener("click", (e) => {
-  myArr = localStorage.getItem("array").split(",");
   if (e.target.id) {
     console.log(e.target.id[e.target.id.length - 1]);
-    delete myArr[e.target.id[e.target.id.length - 1]]
+    delete arr[e.target.id[e.target.id.length - 1]]
     myText.innerHTML = "";
-    myArr.forEach((item, index) => {
+    arr.forEach((item, index) => {
       myText.innerHTML += `<img id="clear${index}" src ="./src/Group77.png"/><li>${item}</li>`;
     });
     if (myText.innerHTML == "") {
       myDiv.style.display = "none";
     }
-    const results = myArr.filter(element => {
+    const results = arr.filter(element => {
       return element !== '';
     });
     localStorage.setItem("array", results);
